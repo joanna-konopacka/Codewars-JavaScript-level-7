eb8157b52782174c90acd573131c44bf94180a3c
Joanna Konopacka <86154250+joanna-konopacka@users.noreply.github.com>
2023-06-18 20:40:17 +0200

    2-day_of_the_year

---
+++ 2-day_of_the_year.js
@@ -0,0 +1,15 @@
+function toDayOfYear(arr) {
+	const [day, month, year] = arr;
+	const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	const leapYear = year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+
+	let countDaysInMonths = 0;
+	let countDaysInYear = 0;
+	for (let i = 0; i < month - 1; i++) {
+		countDaysInMonths += months[i];
+	}
+	countDaysInYear = day + countDaysInMonths;
+	return leapYear && month > 2 ? countDaysInYear + 1 : countDaysInYear;
+}
+
+//const [day, month, year] = arr -destrukturyzacja
