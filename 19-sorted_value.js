cf2ce3fb9467a28cabfc3d0eb67736d7b7b5ac5e
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-08-06 21:33:55 +0200

    task-19-#2solution

--- 19-sorted_value.js
+++ 19-sorted_value.js
@@ -1,5 +1,9 @@
+const solution = (nums) => (nums ? nums.sort((a, b) => a - b) : []);
+
+/*
 function solution(nums) {
 	return nums === null || nums.length === 0
 		? []
 		: [...nums].sort((a, b) => a - b);
 }
+*/
